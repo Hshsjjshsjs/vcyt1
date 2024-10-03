2b3e68c6734b2d2daec7c432ce8e9bf363ef693e
Hshsjjshsjs <arabor0099@gmail.com>
2024-10-03 20:11:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ CHANNEL_ID = os.getenv("CHANNEL_ID")
 SELF_MUTE = os.getenv("SELF_MUTE")
 SELF_DEAF = os.getenv("SELF_DEAF")
 
-usertoken = os.getenv("MTE0ODYwOTY2MjIxNjgzMTAwNg.GJ7YlI.3dSQMDkmQsROQ_g0M_CIchmHd9ATA3fMj-0pMo")
+usertoken = os.getenv("TOKEN")
 if not usertoken:
   print("[ERROR] Please add a token inside Secrets.")
   sys.exit()
